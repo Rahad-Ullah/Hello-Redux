5af97f7a00c5d2b0a9ed170f4de86c0fe822d7b3
Rahad-Ullah <myemail.abc20@gmail.com>
2024-03-01 23:56:59 +0600

    Not

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Counter from "../components/Counter";
 const Home = () => {
     return (
         <div>
-            <h1>Homeee</h1>
+            <h1>Home</h1>
             <Counter/>
         </div>
     );
